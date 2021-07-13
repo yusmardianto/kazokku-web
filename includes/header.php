6a893ab886275d6f48bde25ee5f06a511f40d723
calvinocta <calvin@logique.co.id>
2021-07-13 15:46:57 +0700

    revamp homepage and new page - tentang kami

---
+++ includes/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="keywords"
+      content="web, mobile apps, digital, internet, kazokku"
+    />
+    <meta
+      name="description"
+      content="Kazokku can help you improving or transforming your business from traditional into digital. We are a customer and service oriented Indonesian local company that strive to deliver our best for our clients."
+    />
+    <meta name="author" content="Kazokku Indonesia" />
+    <title>Your Digital and Mobile Solution Partner | Kazokku</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet" />
+    <link href="/css/<?= $css; ?>.css" rel="stylesheet" />
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script>
+      (function (i, s, o, g, r, a, m) {
+        i["GoogleAnalyticsObject"] = r;
+        (i[r] =
+          i[r] ||
+          function () {
+            (i[r].q = i[r].q || []).push(arguments);
+          }),
+          (i[r].l = 1 * new Date());
+        (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+      })(
+        window,
+        document,
+        "script",
+        "https://www.google-analytics.com/analytics.js",
+        "ga"
+      );
+
+      ga("create", "UA-89630965-1", "auto");
+      ga("send", "pageview");
+    </script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <picture>
+            <source srcset="/images/logo-kazokku.svg" type="image/svg+xml">
+            <img src="/images/logo-kazokku.png" alt="">
+          </picture>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-menu">
+          <ul class="navbar-nav mr-0 ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/tentang-kami.php">Tentang Kazok<span class="flip-character">k</span>u</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Layanan Haken</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Hubungi Kami</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,129 @@
+<?php $css = 'home' ?>
+<?php include './includes/header.php' ?>
+<div class="main-visual-section">
+  <picture>
+    <source srcset="">
+    <img src="/images/main-visual.jpg" class="img-main-visual" alt="Main Visual">
+  </picture>
+  <div class="main-visual-text-wrapper">
+    <h1 class="main-visual-text">Mitra Terpercaya Untuk <br/> Layanan Profesional dan Outsourcing IT</h1>
+  </div>
+</div>
+<div class="company-info-section">
+  <img src="/images/bg-dots.png" class="img-dots" alt="dots">
+  <div class="container">
+    <div class="row mb-70px">
+      <div class="col-lg-6">
+        <picture>
+          <source srcset="/images/logique-illustration.webp" type="image/webp">
+          <img src="/images/logique-illustration.jpg" class="img-fluid rounded-10px mb-3" alt="Logique">
+        </picture>
+      </div>
+      <div class="col-lg-6">
+        <h2 class="title head-title">Kazokku merupakan bagian dari Grup <span class="logique-text">LOGIQUE</span> dari Jepang yang dipimpin oleh Takashi Yoshitsugu.</h2>
+        <p>Telah dipercaya banyak Perusahaan dan Profesional dibidang teknologi informasi dalam menyediakan karyawan maupun lapangan pekerjaan terkait Web Developer, Web Designer, Penulis Konten, Ahli SEO dan Pemasaran Online, serta Keamanan Digital, menghubungkan yang terbaik di Indonesia dengan para klien.</p>
+        <a href="#" class="btn btn-orange-rounded">Detail</a>
+      </div>
+    </div>
+    <div class="row mb-70px">
+      <div class="col-lg-4">
+        <picture>
+          <source srcset="/images/haken-service.webp" type="image/webp">
+            <img src="/images/haken-service.jpg" class="img-fluid rounded-10px mb-3" alt="haken service">
+          </picture>
+      </div>
+      <div class="col-lg-8">
+        <h2 class="title">Haken Service</h2>
+        <p class="mb-5">Menanggapi kebutuhan profesional di bidang IT yang semakin dinamis, Kazokku mengembangkan bisnis dari yang awalnya hanya berupa outsourcing IT konvensional berbasis proyek menjadi sebuah layanan Haken atau yang dikenal sebagai dispatching business di Jepang.</p>
+        <p>Dengan adanya layanan Haken, kebutuhan klien akan profesional yang kuat dibidangnya dan mampu mengikuti kebutuhan transformasi digital yang dinamis dapat terpenuhi dengan baik.</p>
+        <a href="#" class="btn btn-orange-rounded">Detail</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <picture>
+          <source srcset="/images/wdi.webp" type="image/webp">
+            <img src="/images/wdi.jpg" class="img-fluid rounded-10px mb-3" alt="haken service">
+          </picture>
+      </div>
+      <div class="col-lg-8">
+        <h2 class="title">WDI</h2>
+        <p class="mb-5">Web Developer Indonesia (WDI) merupakan situs yang dikelola dan dimiliki oleh PT. Karisma Zona Kreatifku (KAZOKKU). Situs ini berfungsi sebagai portal penyedia informasi terlengkap yang menghubungkan antara para perusahaan Web Developer dengan para klien yang membutuhkan. </p>
+        <p>Selain itu, WDI juga berperan sebagai penghubung antara perusahaan Web Developer dengan para pekerja profesional dibidang digital seperti Web Developer, Web Designer, Ahli SEO dan Pemasaran Online, serta Penulis Konten yang sedang membutuhkan pekerjaan.</p>
+
+        <a href="#" class="btn btn-orange-rounded">Detail</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="other-section">
+  <img src="/images/bg-dots.png" class="img-dots" alt="dots">
+  <div class="container">
+    <div class="portofolio-section">
+      <h2 class="title">Portofolio Kami</h2>
+      <div class="portofolio-list">
+        <div class="portofolio-list-item">
+          <img src="/images/sinarmas-logo.png" class="img-portfolio-logo img-sinarmas" alt="sinarmas">
+          <h3 class="portofolio-title">Sinar Mas Land</h3>
+          <p class="mb-2">Kami membantu Sinar Mas Land mengoptimalkan proses rekrutmen dengan menyediakan sistem dan aplikasi yang komprehensif untuk kandidat dan tim rekrutmen yang membuat semua proses berjalan otomatis secara digital.</p>
+          <p class="mb-0">Kebutuhan Teknis : .net, SAP, Active Directory</p>
+        </div>
+        <div class="portofolio-list-item">
+          <img src="/images/bank-building.png" class="img-portfolio-logo img-bank" alt="freen microfinance">
+          <h3 class="portofolio-title">Frenn Microfinance Bank Limited</h3>
+          <p class="mb-2">Kami menyediakan profesional yang memiliki kemampuan sangat baik dalam mengaplikasikan teknologi baru berbasis Go Lang dan mampu berkomunikasi dalam Bahasa Inggris dengan baik untuk berkolaborasi dengan tim internasional dari berbagai negara termasuk India.</p>
+          <p class="mb-0">Kebutuhan Teknis : Go Lang, Microservices</p>
+        </div>
+      </div>
+    </div>
+    <div class="article-section">
+      <h2 class="title">Artikel</h2>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="article-item">
+            <picture>
+              <source srcset="/images/article-1.webp" type="image/webp">
+              <img src="/images/article-1.jpg" class="img-article" alt="article 1">
+            </picture>
+            <h3 class="article-title">Title Artikel One</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <a href="#" class="article-link-detail">Detail <img src="/images/right-arrow-orange.png" class="img-arrow" alt="detail"></a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="article-item">
+            <picture>
+              <source srcset="/images/article-2.webp" type="image/webp">
+              <img src="/images/article-2.jpg" class="img-article" alt="article 2">
+            </picture>
+            <h3 class="article-title">Title Artikel Two</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <a href="#" class="article-link-detail">Detail <img src="/images/right-arrow-orange.png" class="img-arrow" alt="detail"></a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="article-item">
+            <picture>
+              <source srcset="/images/article-3.webp" type="image/webp">
+              <img src="/images/article-3.jpg" class="img-article" alt="article 3">
+            </picture>
+            <h3 class="article-title">Title Artikel Three</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <a href="#" class="article-link-detail">Detail <img src="/images/right-arrow-orange.png" class="img-arrow" alt="detail"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="banner-section">
+      <div class="row w-100">
+        <div class="col-lg-8 d-flex align-items-center">
+          <p class="banner-text">Hubungi Kami Sekarang! <br/> Untuk berkonsultasi dengan Konsultan Rekrutment kami</p>
+        </div>
+        <div class="col-lg-4 d-flex align-items-center justify-content-lg-end">
+          <a href="#" class="btn-orange-rounded mt-3 mt-lg-0 mr-4">Hubungi Kami <img src="/images/right-arrow.png" class="img-arrow" alt="detail"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include './includes/footer.php' ?>
