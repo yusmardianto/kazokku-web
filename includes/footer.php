05ce6c5754f196171ea5bc2a82d1fd14a720a3d8
calvinocta <calvin@logique.co.id>
2021-07-15 13:04:04 +0700

    modify navbar

--- includes/footer.php
+++ includes/footer.php
@@ -80,3 +80,21 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script>
+      window.onscroll = resizeNavbar;
+      window.onload = resizeNavbar;
+
+      function resizeNavbar(){
+        if(document.documentElement.scrollTop > 64){
+          var navbar = document.querySelector('.navbar')
+          var logo = document.getElementById('nav-logo');
+          navbar.style.padding = ".6em 1em";
+          logo.style.width = "90%";
+        } else {
+          var navbar = document.querySelector('.navbar')
+          var logo = document.getElementById('nav-logo');
+          navbar.style.padding = "1em";
+          logo.style.width = "100%";
+        }
+      }
+    </script>
