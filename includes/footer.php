91d2642dde7dd3629ebbf8439244259b97bfd10e
abdul_rachman <abdul.rachman@logique.co.id>
2021-10-12 13:30:19 +0700

    feat(implement-new-design): header

--- includes/footer.php
+++ includes/footer.php
@@ -81,20 +81,20 @@
     <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     <script>
-      window.onscroll = resizeNavbar;
-      window.onload = resizeNavbar;
+      // window.onscroll = resizeNavbar;
+      // window.onload = resizeNavbar;
 
-      function resizeNavbar(){
-        if(document.documentElement.scrollTop > 64){
-          var navbar = document.querySelector('.navbar')
-          var logo = document.getElementById('nav-logo');
-          navbar.style.padding = ".6em 1em";
-          logo.style.width = "90%";
-        } else {
-          var navbar = document.querySelector('.navbar')
-          var logo = document.getElementById('nav-logo');
-          navbar.style.padding = "1em";
-          logo.style.width = "100%";
-        }
-      }
+      // function resizeNavbar(){
+      //   if(document.documentElement.scrollTop > 64){
+      //     var navbar = document.querySelector('.navbar')
+      //     var logo = document.getElementById('nav-logo');
+      //     navbar.style.padding = ".6em 1em";
+      //     logo.style.width = "90%";
+      //   } else {
+      //     var navbar = document.querySelector('.navbar')
+      //     var logo = document.getElementById('nav-logo');
+      //     navbar.style.padding = "1em";
+      //     logo.style.width = "100%";
+      //   }
+      // }
     </script>
