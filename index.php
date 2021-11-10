9171e416b357fe12449e5928d1c8e4897c048d1d
abdul_rachman <abdul.rachman@logique.co.id>
2021-11-10 16:37:23 +0700

    fix: style

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <div class="col-sm-12 col-md-6 col-md-6 col-lg-6 col-xl-6 ">    
       <picture>
         <source srcset="/images/home/image-section-1-circle-outline.png" type="image/png" >
-        <img class="ml-n3 " width="20" height="20" src="/image-section-1-circle-outline.png" alt="complementary" />
+        <img class="ml-n3 " width="20" height="20" src="/images/home/image-section-1-circle-outline.png" alt="complementary" />
       </picture>                
       <h1>
         Mitra Terpercaya Untuk <br />
@@ -28,20 +28,20 @@
       </h1>  
       <picture>
         <source srcset="/images/home/image-2-section-1.png" type="image/png" >
-        <img class="mt-3 " width="112" height="27" src="/images/home/image-2-section-1.svg" alt="complementary" />
+        <img class="mt-3" width="112" height="27" src="/images/home/image-2-section-1.png" alt="complementary" />
       </picture>        
     </div>
     <div class="col-sm-12 col-md-6 col-md-6 col-lg-6 col-xl-6 text-center">     
       <div class="mt-n7">                       
           <picture>
-              <source media="(min-width: 1024px)" srcset="/images/home/image-section-1-orange-man.png">              
-              <img class="w-sm-100 mt-sm-4 " src="/images/home/image-section-1-orange-man.png" alt="section-1"  loading="lazy" >
+              <source media="(min-width: 1024px)" srcset="/images/home/image-section-1-orange-man.PNG">              
+              <img class="w-sm-100 mt-sm-4" src="/images/home/image-section-1-orange-man.PNG" alt="section-1" loading="lazy" >
           </picture>  
           <!-- <picture>
               <source media="(min-width: 1024px)" srcset="/images/home/image-1-section-1.svg">
               <source media="(max-width: 640px)" srcset="/images/home/image-1-section-1.svg">
               <img class="w-sm-100 mt-sm-4 " src="/images/home/image-section-1.png" alt="section-1"  loading="lazy" >
-          </picture>                   -->
+          </picture>  -->
       </div>                 
     </div>
   </div>
@@ -92,7 +92,7 @@
                 WHO WE ARE
             </h4>        
             <h3 class="mt-3 mb-3">
-              Kazokku merupakan bagian dari Grup logique dari Jepang yang dipimpin oleh Takashi Yoshitsugu.
+              Kazokku merupakan bagian dari Grup <span class="text-secondary-main logique-text">LOGIQUE</span> dari Jepang yang dipimpin oleh Takashi Yoshitsugu.
             </h3>                                         
             <p>
               Telah dipercaya banyak Perusahaan dan Profesional dibidang teknologi informasi dalam menyediakan karyawan maupun lapangan pekerjaan terkait Web Developer, Web Designer, Penulis Konten, Ahli SEO dan Pemasaran Online, serta Keamanan Digital, menghubungkan yang terbaik di Indonesia dengan para klien.
@@ -212,54 +212,62 @@
 
 
 <!-- #5 -->  
-<section class="container mt-5">
-  <div class="row ">
-    <div class="col-sm-12 col-md-12 col-md-12 col-lg-12 col-xl-12">
-        <h4 class="text-secondary-main mt-4">
-            WHAT WE DO
-        </h4>
-        <h3 class="mt-3 mb-3">
-          Portofolio Kami
-        </h3> 
+<section>
+  <div  class="container mt-5 pb-5">
+    <div class="row ">
+      <div class="col-sm-12 col-md-12 col-md-12 col-lg-12 col-xl-12">
+          <h4 class="text-secondary-main mt-4">
+              WHAT WE DO
+          </h4>
+          <h3 class="mt-3 mb-3">
+            Portofolio Kami
+          </h3> 
+      </div>
     </div>
-  </div>
-
-  <div class="row mt-3">
-    <div class="col-sm-12 col-md-6 col-md-6 col-lg-6 col-xl-6 " >      
-      <fieldset class="card-info-with-fieldset h-100" style="background-color: rgb(244 209 215); background-image:linear-gradient(162deg, rgba(225,111,132,0.10688025210084029) 21%, rgb(231 243 255) 69%);">
-          <legend class="card-info-with-fieldset" >
-              <span class="fs-6">Logo</span>
-          </legend>
-          <div class="control-group">
-              <h5 class="mt-5">
-                <b>Sinar Mas Land</b>
-              </h5>
-              <p>
-                Kami membantu Sinar Mas Land mengoptimalkan proses rekrutmen dengan menyediakan sistem dan aplikasi yang komprehensif untuk kandidat dan tim rekrutmen yang membuat semua proses berjalan otomatis secara digital.
-              </p>
-              <p>
-                Kebutuhan Teknis : .net, SAP, Active Directory
-              </p>  
-          </div>
-        </fieldset>  
-    </div>    
-    <div class="col-sm-12 col-md-6 col-md-6 col-lg-6 col-xl-6 " >      
-      <fieldset class="card-info-with-fieldset h-100" style="background-color: rgb(244 209 215); background-image:linear-gradient(162deg, rgba(225,111,132,0.10688025210084029) 21%, rgb(231 243 255) 69%);">
-          <legend class="card-info-with-fieldset" >
-              <span class="fs-6">Logo</span>
-          </legend>
-          <div class="control-group">
-              <h5 class="mt-5">
-                <b>Frenn Microfinance Bank Limited</b>
-              </h5>
-              <p>
-                Kami menyediakan profesional yang memiliki kemampuan sangat baik dalam mengaplikasikan teknologi baru berbasis Go Lang dan mampu berkomunikasi dalam Bahasa Inggris dengan baik untuk berkolaborasi dengan tim internasional dari berbagai negara termasuk India.
-              </p>
-              <p>
-                Kebutuhan Teknis : Go Lang, Microservices
-              </p> 
-          </div>
-        </fieldset>  
+  
+    <div class="row mt-3">
+      <div class="col-sm-12 col-md-6 col-md-6 col-lg-6 col-xl-6 " >      
+        <fieldset id='home-card-portofolio-kami-1' class="card-info-with-fieldset h-100" style="background-color: rgb(244 209 215); ">
+            <legend class="card-info-with-fieldset" >
+                <span class="fs-6">
+                  <img height="48" width="48" src="/images/home/image-logo-sinarmas-land.png" alt="logo-sinarmas-land"  loading="lazy" >
+                </span>
+            </legend>
+            <div class="control-group">
+                <h5 class="mt-5">
+                  <b>Sinar Mas Land</b>
+                </h5>
+                <p>
+                  Kami membantu Sinar Mas Land mengoptimalkan proses rekrutmen dengan menyediakan sistem dan aplikasi yang komprehensif untuk kandidat dan tim rekrutmen yang membuat semua proses berjalan otomatis secara digital.
+                </p>
+                <p>
+                  Kebutuhan Teknis : .net, SAP, Active Directory
+                </p>  
+            </div>
+          </fieldset>  
+      </div>    
+      <div class="col-sm-12 col-md-6 col-md-6 col-lg-6 col-xl-6 " >      
+        <fieldset id='home-card-portofolio-kami-2' class="card-info-with-fieldset h-100" style="background-color: rgb(244 209 215); ">
+            <legend class="card-info-with-fieldset" >
+                <span class="fs-6">
+                  <div class="rounded" style='background-color: #008247; height: 48px; width: 48px;'>
+                    <img class="p-2 " style="object-position: 50% 50%; object-fit: cover;" height="100%" width="100%" src="/images/home/image-logo-limited-bank.png" alt="logo-limited-bank"  loading="lazy" >
+                  </div>
+                </span>
+            </legend>
+            <div class="control-group">
+                <h5 class="mt-5">
+                  <b>Frenn Microfinance Bank Limited</b>
+                </h5>
+                <p>
+                  Kami menyediakan profesional yang memiliki kemampuan sangat baik dalam mengaplikasikan teknologi baru berbasis Go Lang dan mampu berkomunikasi dalam Bahasa Inggris dengan baik untuk berkolaborasi dengan tim internasional dari berbagai negara termasuk India.
+                </p>
+                <p>
+                  Kebutuhan Teknis : Go Lang, Microservices
+                </p> 
+            </div>
+          </fieldset>  
+      </div>
     </div>
   </div>
 </section>
